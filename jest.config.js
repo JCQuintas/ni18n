b9b2774512f5fa163868c5ab062543f4a485d821
JCQuintas <juniorquintas@gmail.com>
2021-07-03 19:44:02 +0200

    fix jest complaints

--- jest.config.js
+++ jest.config.js
@@ -2,5 +2,5 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   collectCoverageFrom: ['src/**/*.{ts,tsx}', '!src/**/index.{ts,tsx}'],
-  testPathIgnorePatterns: ['<rootDir>/{dist,examples}'],
+  modulePathIgnorePatterns: ['<rootDir>/dist'],
 }
