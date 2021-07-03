7f1796d1f6ec6694f1ad16d3319b9d6598fed7d7
JCQuintas <juniorquintas@gmail.com>
2021-07-03 23:00:27 +0200

    fix jest config to find use-backend

--- jest.config.js
+++ jest.config.js
@@ -3,4 +3,7 @@ module.exports = {
   testEnvironment: 'node',
   collectCoverageFrom: ['src/**/*.{ts,tsx}', '!src/**/index.{ts,tsx}'],
   modulePathIgnorePatterns: ['<rootDir>/dist'],
+  moduleNameMapper: {
+    'use-backend': '<rootDir>/src/use-backend/browser.ts',
+  },
 }
