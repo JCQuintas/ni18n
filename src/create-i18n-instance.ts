64bf100f7b8bb33b25b1212182fb7c4771bda73f
JCQuintas <juniorquintas@gmail.com>
2021-07-03 18:24:31 +0200

    divide create instance file

--- src/create-i18n-instance.ts
+++ src/create-i18n-instance.ts
@@ -2,30 +2,8 @@ import type { InitOptions, i18n as I18NextClient } from 'i18next'
 import { initReactI18next } from 'react-i18next'
 import i18n from 'i18next'
 import useBackend from './use-backend'
-
-const isBrowser = () => typeof window !== 'undefined'
-const getBackendConfig = (options: InitOptions) => {
-  const hasCustomBackend = options.backend
-
-  // Server side backend config
-  if (!isBrowser() && !hasCustomBackend) {
-    // eslint-disable-next-line @typescript-eslint/no-var-requires
-    const path = require('path')
-    const localePath = './public/locales/{{lng}}/{{ns}}'
-
-    return {
-      backend: {
-        addPath: path.resolve(process.cwd(), `${localePath}.missing.json`),
-        loadPath: path.resolve(process.cwd(), `${localePath}.json`),
-      },
-    }
-  }
-
-  // Client side backend config
-  return {
-    preload: options.supportedLngs,
-  }
-}
+import { getBackendConfig } from './get-backend-config'
+import { isBrowser } from './is-browser'
 
 export const createI18nInstance = (options: InitOptions): I18NextClient => {
   const config: InitOptions = {
---
+++ src/get-backend-config.ts
@@ -0,0 +1,27 @@
+import { InitOptions } from 'i18next'
+import { isBrowser } from './is-browser'
+
+export const getBackendConfig = (
+  options: InitOptions,
+): Record<string, unknown> => {
+  const hasCustomBackend = options.backend
+
+  // Server side backend config
+  if (!isBrowser() && !hasCustomBackend) {
+    // eslint-disable-next-line @typescript-eslint/no-var-requires
+    const path = require('path')
+    const localePath = './public/locales/{{lng}}/{{ns}}'
+
+    return {
+      backend: {
+        addPath: path.resolve(process.cwd(), `${localePath}.missing.json`),
+        loadPath: path.resolve(process.cwd(), `${localePath}.json`),
+      },
+    }
+  }
+
+  // Client side backend config
+  return {
+    preload: options.supportedLngs,
+  }
+}
