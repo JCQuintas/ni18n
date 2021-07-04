4bc0225b1f3469f16b74c192d150514039c0f11d
JCQuintas <juniorquintas@gmail.com>
2021-07-05 00:26:29 +0200

    fix custom location usage

--- src/use-backend/server.test.ts
+++ src/use-backend/server.test.ts
@@ -9,10 +9,18 @@ it('should call the use function', () => {
   expect(use).toHaveBeenCalledWith(expect.anything())
 })
 
-it('should not call the use function if modules.backend exists', () => {
+it('should not call the use function if there is a custom backend plugin', () => {
   const use = jest.fn()
 
-  useBackend({ use, modules: { backend: {} } } as unknown as i18n)
+  useBackend({ use } as unknown as i18n, [{ type: 'backend' }])
+
+  expect(use).not.toHaveBeenCalled()
+})
+
+it('should not call the use function if there is a custom backend plugin inside an array', () => {
+  const use = jest.fn()
+
+  useBackend({ use } as unknown as i18n, [[{ type: 'backend' } as never]])
 
   expect(use).not.toHaveBeenCalled()
 })
--- src/use-backend/server.ts
+++ src/use-backend/server.ts
@@ -1,8 +1,17 @@
 import type { i18n as I18NextClient } from 'i18next'
 import FSBackend from 'i18next-fs-backend'
 
-export default (i18n: I18NextClient): I18NextClient => {
-  const hasCustomBackend = i18n.modules.backend
+type Type = { type: string }
+
+export default (
+  i18n: I18NextClient,
+  plugins?: Parameters<I18NextClient['use']>[0][],
+): I18NextClient => {
+  const hasCustomBackend = plugins?.some((plugin) =>
+    Array.isArray(plugin)
+      ? plugin.some((p) => (p as Type).type === 'backend')
+      : (plugin as Type).type === 'backend',
+  )
   if (!hasCustomBackend) {
     i18n.use(FSBackend)
   }
