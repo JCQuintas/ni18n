4793c468a3b7a6ec44f0606a0a23e08668dd6ff5
JCQuintas <juniorquintas@gmail.com>
2021-07-21 01:26:23 +0200

    chore: make alternate page server side

--- examples/typescript/pages/alternate.tsx
+++ examples/typescript/pages/alternate.tsx
@@ -3,7 +3,7 @@ import { Footer } from '../components/footer'
 import { useTranslation } from 'react-i18next'
 import { loadTranslations } from 'ni18n'
 import { ni18nConfig } from '../ni18n.config'
-import { GetStaticProps } from 'next'
+import { GetServerSideProps } from 'next'
 
 const AlternatePage = () => {
   const { t } = useTranslation('alternate')
@@ -20,7 +20,7 @@ const AlternatePage = () => {
   )
 }
 
-export const getStaticProps: GetStaticProps = async (props) => {
+export const getServerSideProps: GetServerSideProps = async (props) => {
   return {
     props: {
       ...(await loadTranslations(ni18nConfig, props.locale, [
