e3e89a07af58ff5e61f53d3ed8d95f1b7173b417
JCQuintas <juniorquintas@gmail.com>
2021-07-22 23:04:32 +0200

    fix: export types from common

--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,9 @@ export { appWithI18Next } from './app-with-i18next'
 export { loadTranslations } from './load-translations'
 export { useSyncLanguage } from './use-sync-language'
 export { clientNamespaces } from './client-namespaces'
-export type { Ni18nOptions } from './common'
+export type {
+  Ni18nOptions,
+  Ni18nClientState,
+  Ni18nServerState,
+  NamespacesNeeded,
+} from './common'
