7e1b3630b78a5755f693d5ba21862859c6d316ca
JCQuintas <juniorquintas@gmail.com>
2021-10-18 22:02:45 +0200

    chore: update packages and test script

--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,12 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   collectCoverageFrom: ['src/**/*.{ts,tsx}', '!src/**/index.{ts,tsx}'],
-  modulePathIgnorePatterns: ['<rootDir>/dist', '<rootDir>/e2e'],
+  modulePathIgnorePatterns: [
+    '<rootDir>/dist',
+    '<rootDir>/e2e',
+    '<rootDir>/data',
+    '<rootDir>/examples',
+  ],
   moduleNameMapper: {
     'use-backend': '<rootDir>/src/use-backend/browser.ts',
   },
