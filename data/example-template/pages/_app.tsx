b44d2dce1846428226c795f25b8d266e40aa1bc1
JCQuintas <juniorquintas@gmail.com>
2021-11-05 23:30:43 +0100

    chore:update next 12

--- data/example-template/pages/_app.tsx
+++ data/example-template/pages/_app.tsx
@@ -1,7 +1,10 @@
 import '../public/styles.css'
 import { appWithI18Next } from 'ni18n'
 import { ni18nConfig } from '../ni18n.config'
+import type { AppProps } from 'next/app'
 
-const MyApp = ({ Component, pageProps }) => <Component {...pageProps} />
+const MyApp = ({ Component, pageProps }: AppProps) => (
+  <Component {...pageProps} />
+)
 
 export default appWithI18Next(MyApp, ni18nConfig)
