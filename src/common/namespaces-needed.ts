af5989c98adee9c8f06c9c6dd69d71d03d2bfeca
Jose Quintas <juniorquintas@gmail.com>
2023-03-05 13:32:43 +0100

    fix: namespace import from i18next instead of react-i18next

--- src/common/namespaces-needed.ts
+++ src/common/namespaces-needed.ts
@@ -1,4 +1,4 @@
-import type { Namespace } from 'react-i18next'
+import type { Namespace } from 'i18next'
 
 // For some reason if we don't reassign `Namespace` it gets compiled into `Namespace<string>`
 export type NamespacesNeeded = Namespace
